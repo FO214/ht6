ec97a48650a4faea2367faff4d66cf3f79df0d4c
Fahmi Omer <fahmiomer02@gmail.com>
2024-08-03 06:34:12 -0400

    db

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from db.main import update, get_hp
+from flask_cors import CORS
 import time
 from arduino.read_serial import get_hardware_data
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/sensor-data', methods=['GET'])
 def get_sensor_data():
@@ -19,6 +23,44 @@ def get_sensor_data():
             return jsonify({'error': 'Failed to read sensor data'}), 500
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+    
+
+# return json of dimness and moistness
+@app.route("/battle-stats", methods=["GET"])
+def get_battle_stats():
+    try:
+        data = request.get_json()
+        usr = data["user"]
+
+
+    except Exception as e:
+        return e
+
+
+@app.route("/get-battle-update", methods=["GET"])
+def get_battle_updates():
+    try:
+        data = request.get_json()
+        usr = "P2" if data["user"] == "P1" else "P1"
+        return get_hp(usr)
+
+    except Exception as e:
+        return e
+
+
+@app.route("/attack", methods=["POST"])
+def attack_plant():
+    try:
+        data = request.get_json()
+        usr = data["user"]
+        dmg = data['damage']
+        update(usr, dmg)
+
+        return "success"
+    except Exception as e:
+        return e
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
