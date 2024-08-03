215ccb4e6d04e3907d22f693387ce989a3387713
sbrina-w <sabrinawang93@gmail.com>
2024-08-03 08:20:26 -0400

    battle screen ui layout

--- frontend/pokeplants/src/components/battlescreen/battlescreen.js
+++ frontend/pokeplants/src/components/battlescreen/battlescreen.js
@@ -7,10 +7,15 @@ const BattleScreen = () => {
     <div className="battle-screen">
       <div className="player-plant">
         <div className="plant-info" id="player-plant-info">
-          <span>Inch Plant</span>
-          <span>Lvl 25</span>
+          <div className="plant-name-lvl">
+            <span>Inch Plant</span>
+            <span>Lvl 25</span>
+          </div>
           <div className="health-bar">
             <div className="health"></div>
+          </div>
+          <div class="hp-status">
+            <span>HP</span> <span>78/122</span>
           </div>
         </div>
         <div className="player-plant-battler">
@@ -19,10 +24,15 @@ const BattleScreen = () => {
       </div>
       <div className="opponent-plant">
         <div className="plant-info" id="opponent-plant-info">
-          <span>Spider Plant</span>
-          <span>Lvl 28</span>
+          <div className="plant-name-lvl">
+            <span>Spider Plant</span>
+            <span>Lvl 28</span>
+          </div>
           <div className="health-bar">
             <div className="health"></div>
+          </div>
+          <div class="hp-status">
+            <span>HP</span> <span>78/122</span>
           </div>
         </div>
         <div className="opponent-plant-battler">
@@ -32,15 +42,17 @@ const BattleScreen = () => {
      
     </div>
     <div className="attack-screen">
-    <div className="actions">
+      <div className="actions">
         <button>Attack 1</button>
         <button>Attack 2</button>
         <button>Attack 3</button>
         <button>Attack 4</button>
       </div>
       <div className="status">
-        <span>Status: </span>
-        <span>Disarmed</span>
+        <div className="status-effects">
+          <span>Status: </span>
+          <span>Poisoned</span>
+        </div>
       </div>
     </div>
     </div>
